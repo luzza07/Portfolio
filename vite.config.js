84553ecbab2cc2cbe5b65d3dc43c6979c26f36cc
luzza07 <luzzadangol456@gmail.com>
2025-04-30 19:59:32 +0545

    Fix base path and update title

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio-Website/', // Replace with your repository name
+  base: '/Portfolio/', // Replace with your repository name
 });
