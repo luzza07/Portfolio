156c107b65b70aceee206f4fbc7051ce271eaa10
luzza07 <luzzadangol456@gmail.com>
2025-04-30 20:09:41 +0545

    socket fixed

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/', 
+  server: {
+    hmr: process.env.NODE_ENV === 'development', 
+  },
 });
